88deca92fb9edfa2d811cba63aa1f28ae86f4c7a
5saito <dj--x@i.softbank.jp>
2020-08-26 20:20:01 +0900

    ユーザー情報のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, {presence: true, uniqueness:true}
+  validates :password, {presence: true, length: {minimum: 6}}
+  validates :birth_day, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 end
