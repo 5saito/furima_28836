14fae6cfe3a5a251fd2b30e8b3c1a636366ff63b
5saito <dj--x@i.softbank.jp>
2020-08-27 15:33:28 +0900

    新規登録/ログイン/ログアウトまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :email, {presence: true, uniqueness:true}
+  validates :email, presence: true
   validates :password, {presence: true, length: {minimum: 6}}
   validates :birth_day, presence: true
 
