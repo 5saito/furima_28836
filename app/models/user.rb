acc313f2fbe3d47998960017af29cec6a09cf0b5
5saito <dj--x@i.softbank.jp>
2020-08-28 19:22:41 +0900

    エラーメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGAX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGAX , allow_blank: true}
   validates :password, {presence: true, length: {minimum: 6}}
   validates :password, confirmation: true
   validates :birth_day, presence: true
