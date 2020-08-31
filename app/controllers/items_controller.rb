18e2daa29e1fb2155da0e706803360e359ae1154
5saito <dj--x@i.softbank.jp>
2020-08-31 17:57:02 +0900

    ログインしているユーザーだけが、出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  
+  def new
+    @item = Item.new
+  end
   private
 
   def items_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:text, :name, :image, :price, :category, 
+    :prefecture, :condition, :days, :delivery_fee, :user).merge(user_id: current_user.id)
   end
 end
