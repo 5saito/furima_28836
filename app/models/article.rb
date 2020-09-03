5806422cd10bf3458ad9a63be8a5093de8863f4a
5saito <dj--x@i.softbank.jp>
2020-09-03 12:30:49 +0900

    コメントアウトの削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,2 @@
 class Article < ApplicationRecord
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :genre
-  # validates :name, :text, :genre, presence: true
-  # validates :genre_id, numericality: { other_than: 1 } 
 end
