9c537daec73f29ae86aa5656d02ca0ce97fff261
5saito <dj--x@i.softbank.jp>
2020-09-07 16:49:32 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'articles/index'
   get 'articles/new'
   devise_for :users
-  root to: "items#index"
+  root to: 'orders#index'
+  resources :orders, only:[:create]
   resources :users, only: [:edit, :update, :create, :destroy, :new]
   resources :items
   resources :articles, only:[:index, :new, :create]
