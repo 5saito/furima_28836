233fd39c6e312c763e71c9be060a9bcb3b9b19eb
5saito <dj--x@i.softbank.jp>
2020-09-10 21:29:47 +0900

    売れたものにはsoldoutの表示を行う

--- spec/factories/items_purchases.rb
+++ spec/factories/items_purchases.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :items_purchase do
-    post_cord = Faker::Name.post_cord(min_length: 8)
+    post_cord {Faker::Name.post_cord(min_length: 8)}
     city {Faker::Name.city}
     address {Faker::Name.address}
-    phone_number = Faker::Name.phone_number(min_length: 11)
+    phone_number {Faker::Name.phone_number(min_length: 11)}
     prefecture_id {Faker::Name.prefecture_id}
     user_id {Faker::Name.user_id}
     item_id {Faker::Name.item_id}
