e964de4bbdb41fa7becec027e71cd22e6720c538
5saito <dj--x@i.softbank.jp>
2020-09-12 20:39:20 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,15 @@ class Item < ApplicationRecord
   has_one :items_purchase
   has_one_attached :image
 
-
-
-
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+    validates :category_id,  numericality: { other_than: 1} 
+    validates :image
+    validates :condition_id,  numericality: { other_than: 1} 
+    validates :prefecture_id,  numericality: { other_than: 1} 
+    validates :days_id,  numericality: { other_than: 1} 
+    validates :delivery_fee_id,  numericality: { other_than: 1} 
+ end
 end
